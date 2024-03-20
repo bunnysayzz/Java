3e6e61f809be66d99a939294082bbd01184ea942
bunnysayzz <stfuazzo@gmail.com>
2024-03-21 01:16:36 +0530

    Primitive

---
+++ Primitive data types/Primitive.java
@@ -0,0 +1,12 @@
+public class Primitive {
+    public static void main(String[] args){
+        byte myByte = 12;
+        float myFloat = 3.1234f;
+        boolean myBoolean = true;
+
+        System.out.println(myByte);
+        System.out.println(myFloat);
+        System.out.println(myBoolean);
+
+    }
+}
