5a466f5b60eb8d77a30c75a2c66aa936a871d027
bunnysayzz <stfuazzo@gmail.com>
2024-03-21 10:22:04 +0530

    type casting

---
+++ Type Casting/Explicit.java
@@ -0,0 +1,13 @@
+public class Explicit{
+    public static void main(String[] args){
+
+        int a =(int) 3.7;
+        System.out.println(a);
+        int b =(int) 43L;
+        System.out.println(b);
+
+        float c =(float) 2.4;
+        System.out.println(c);
+
+
+    }}
---
+++ Type Casting/Implicit.java
@@ -0,0 +1,8 @@
+public class Implicit{
+    public static void main(String[] args){
+        double a = 4; //int value assigned in double data types
+        float b = 3.44f; //decimal value inside float data types 
+        long c = 123; //int value in int data types
+
+    }
+}
