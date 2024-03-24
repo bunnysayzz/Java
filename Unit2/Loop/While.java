7c9fda24094bfa4c934948ff38691340ad2021fa
bunnysayzz <stfuazzo@gmail.com>
2024-03-24 21:40:26 +0530

    Loop

---
+++ Unit2/Loop/While.java
@@ -0,0 +1,16 @@
+public class While{
+    public static void main(String[] args){
+        
+        //initialization
+        int i=1;
+        
+        //condition
+        while(i<=5)
+        {
+            System.out.println("Hey this is bunny " + " " + i);
+
+            //increment
+            i++;
+        }
+    }
+}
