1ca71aceef840e7fb7249e98e461aff5e96c7ef0
bunnysayzz <stfuazzo@gmail.com>
2024-03-25 00:13:13 +0530

    dowhile

---
+++ Unit2/Loop/DoWhile.java
@@ -0,0 +1,14 @@
+public class DoWhile{
+    public static void main(String[] args){
+        int i=5;
+        do{
+            System.out.println("Hey bunny");
+            i++;
+        }
+        //do while assures that atleast once its executed than it checks the condition
+
+        //condition
+        while(i<=4);
+    }
+}
+
