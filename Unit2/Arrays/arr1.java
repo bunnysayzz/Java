c9895897e26cca9bffddb490db15b8b77ee2c957
bunnysayzz <stfuazzo@gmail.com>
2024-03-27 22:00:30 +0530

    array

---
+++ Unit2/Arrays/arr1.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class arr1{
+    public static void main(String[] args){
+        // int[] arr = {2,4,5,6};
+        int[] arr = new int[5];
+        Scanner scanner = new Scanner(System.in);
+        for (int i=0; i<5; i++){
+            arr[i] = scanner.nextInt();
+
+
+        }
+        System.out.println(Arrays.toString(arr));
+
+        //for each loop
+        for(int num : arr)
+        System.out.println(num);
+    }
+}
